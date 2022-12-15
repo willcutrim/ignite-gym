bacaa7a5307b3957c65ed3bf8b9482347c26437f
willcutrim <willyam894@gmail.com>
2022-12-15 15:12:14 -0300

    seção de histórico feita

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
-import { AuthRoutes } from './auth.routes';
 import { AppRoutes } from './app.routes';
 
 export function Routes(){
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,45 @@
-import { Center, Text } from 'native-base';
+import { VStack, Heading, SectionList, Text } from 'native-base';
+import { HistoryCard } from '@components/HistoryCard';
+import { ScreenHeader } from '@components/ScreenHeader';
+import { useState } from 'react';
 
 export function History(){
+
+    const [exercicios, setExercicios] = useState([
+            {
+                title: "26.08.22",
+                data: ["Puxada frontal", "Remada unilateral"], 
+            },
+            {
+                title: "28.08.22",
+                data: ["Puxada frontal"], 
+            },
+    ]);
+
     return(
-        <Center flex={1}>
-            <Text color="white">
-                History
-            </Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Histórico de Exercícios"/>
+
+            <SectionList 
+                sections={exercicios}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <HistoryCard/>
+                )}
+                renderSectionHeader={({ section }) => (
+                    <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+                        {section.title}
+                    </Heading>
+                )}
+                px={8}
+                contentContainerStyle={exercicios.length === 0 ? {flex: 1, justifyContent:"center"} : null}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center">
+                        Não há exercícios registrados ainda. {'\n'}
+                        Vamos fazer exercícios hoje?
+                    </Text>
+                )}
+            />
+        </VStack>
     );
 }
