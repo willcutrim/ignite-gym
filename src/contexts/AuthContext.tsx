0f1b04e48ffdc11eb13b264c1506a090c9ae5ce0
willcutrim <willyam894@gmail.com>
2022-12-23 10:23:04 -0300

    implementando autenticação JWT

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,13 +1,14 @@
 import { ReactNode, createContext, useEffect, useState } from 'react'
 
 import { storageUserSave, storageUserGet, storageUserDelete } from '@storage/storageUser';
+import { storageAuthTokenSave, storageAuthTokenGet, storageAuthTokenDelete } from '@storage/storageAuthToken';
 
 import { api } from '@services/api';
 import { UserDTO } from '@dtos/UserDTOS';
 
 export type AuthContextDataProps = {
     user: UserDTO;
-    signIn: ( email: string, password: string ) => Promise<void>;
+    signIn: (email: string, password: string) => Promise<void>;
     signOut: () => Promise<void>;
     isLoadingUserStorageData: boolean;
 }
@@ -19,27 +20,28 @@ type AuthContextPRoviderProps = {
 }
 
 
+
 export function AuthContextProvider({ children }: AuthContextPRoviderProps) {
     const [user, setUser] = useState<UserDTO>({} as UserDTO);
-    const [ isLoadingUserStorageData, setIsLoadingUserStorageData ] = useState(true);
+    const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
-    async function signIn( email: string, password: string){
+    async function userAndTokenUpdate(userData: UserDTO, token: string) {
         try {
-	        const { data } = await api.post('/sessions', { email, password });
-            if(data.user){
-                setUser(data.user);
-                storageUserSave(data.user);
-            }
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            setUser(userData);
+
         } catch (error) {
             throw error;
         }
     }
 
-    async function signOut(){
+    async function storageUserAndTokenSave(userData: UserDTO, token: string) {
         try {
             setIsLoadingUserStorageData(true);
-            setUser({} as UserDTO);
-            await storageUserDelete();
+
+            await storageUserSave(userData);
+            await storageAuthTokenSave(token);
         } catch (error) {
             throw error;
         } finally {
@@ -47,12 +49,14 @@ export function AuthContextProvider({ children }: AuthContextPRoviderProps) {
         }
     }
 
-    async function loadUserData(){
+    async function signIn(email: string, password: string) {
         try {
-            const userLogged = await storageUserGet();
-    
-            if(userLogged){
-                setUser(userLogged);
+            const { data } = await api.post('/sessions', { email, password });
+            if (data.user && data.token) {
+                await storageUserAndTokenSave(data.user, data.token);
+
+                userAndTokenUpdate(data.user, data.token);
+
             }
         } catch (error) {
             throw error;
@@ -61,13 +65,44 @@ export function AuthContextProvider({ children }: AuthContextPRoviderProps) {
         }
     }
 
-    useEffect(() =>{
+    async function signOut() {
+        try {
+            setIsLoadingUserStorageData(true);
+            
+            setUser({} as UserDTO);
+            await storageUserDelete();
+            await storageAuthTokenDelete();
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    async function loadUserData() {
+        try {
+            setIsLoadingUserStorageData(true);
+
+            const userLogged = await storageUserGet();
+            const token = await storageAuthTokenGet();
+
+            if (token && userLogged) {
+                userAndTokenUpdate(userLogged, token);
+            }
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingUserStorageData(false);
+        }
+    }
+
+    useEffect(() => {
         loadUserData();
-    },[])
+    }, [])
 
     return (
-        <AuthContext.Provider value={{ 
-            user, 
+        <AuthContext.Provider value={{
+            user,
             signIn,
             signOut,
             isLoadingUserStorageData
